97848a2eb12c89004ca8b5dc6cc3f7f0cafdbd50
Mehrdad Pazooki <mehrdad@tranquant.com>
2016-08-22 11:17:02 -0400

    back to old api endpoint

--- tqcli/tqcli.py
+++ tqcli/tqcli.py
@@ -1,9 +1,9 @@
-from tqcli.config.config import TQ_API_ROOT_URL, LOG_PATH
-from tqcli.batch.server_handler import TranQuant
-
+import os
 import optparse
 import logging
-import os
+
+from tqcli.config.config import TQ_API_ROOT_URL, LOG_PATH
+from tqcli.batch.server_handler import TranQuant
 
 logger = logging.getLogger(os.path.basename(__file__))
 
