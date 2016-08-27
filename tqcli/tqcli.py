b3e885a68898ed9525ed75610068ee9b0139d84e
Mehrdad Pazooki <mehrdad@tranquant.com>
2016-08-27 12:54:36 -0400

    conditional import for config

--- tqcli/tqcli.py
+++ tqcli/tqcli.py
@@ -2,10 +2,10 @@ import optparse
 import logging
 import os
 
-try:
+try: # for Python3
     from tqcli.config.config import TQ_API_ROOT_URL, LOG_PATH
     from tqcli.batch.server_handler import TranQuant
-except ImportError:
+except ImportError: # for Python27
     from config.config import TQ_API_ROOT_URL, LOG_PATH
     from batch.server_handler import TranQuant
 
